21d20e3ed902e0578b89c6d848461de37fe39f15
gourabbank <gourabbank12345@gmail.com>
2024-05-13 11:35:54 -0400

    Initial Commit

---
+++ timepass/timepassApp.swift
@@ -0,0 +1,17 @@
+//
+//  timepassApp.swift
+//  timepass
+//
+//  Created by Gourab Bank on 13/5/24.
+//
+
+import SwiftUI
+
+@main
+struct timepassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
