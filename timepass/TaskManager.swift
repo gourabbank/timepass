3b88f3a726500ea31d2c67c0e1163401e58ee7d2
gourabbank <gourabbank12345@gmail.com>
2024-05-18 02:14:37 +0530

    Made TaskManager Swift Code

---
+++ timepass/TaskManager.swift
@@ -0,0 +1,60 @@
+//
+//  TaskManager.swift
+//  timepass
+//
+//  Created by Gourab Bank on 18/5/24.
+//
+
+import Foundation
+class TaskManager: ObservableObject
+{
+    @Published var tasks:[Task]=[]
+    {
+        didSet
+        {
+            saveTasks()
+        }
+    }
+    
+    init()
+    {
+        loadTasks()
+    }
+    
+    func addTasks(title:String)
+    {
+        let task = Task(title:title)
+        tasks.append(task)
+    }
+    
+    func toggleTaskCompletion(task:Task)
+    {
+        if let index=tasks.firstIndex(where: {$0.id==task.id})
+        {
+            tasks[index].isCompleted.toggle()
+        }
+    }
+    
+    func deleteTasks(at offsets:IndexSet)
+    {
+        tasks.remove(atOffsets: offsets)
+    }
+    
+    private func saveTasks()
+    {
+        if let encoded = try? JSONEncoder().encode(tasks)
+        {
+            UserDefaults.standard.set(encoded, forKey: "tasks")
+        }
+    }
+    
+    private func loadTasks()
+    {
+        if let saveTasks = UserDefaults.standard.data(forKey: "tasks"),
+           let decodedTasks = try? JSONDecoder().decode([Task].self, from: saveTasks)
+        {
+            tasks=decodedTasks
+        }
+            
+    }
+}
